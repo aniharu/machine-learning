900636cf8ac832d1297bf5d2e66c501164fd9de6
aniharu <haruya04183083@gmail.com>
2016-05-25 14:11:01 +0900

    Add files via upload

--- Cross_validation.py
+++ Cross_validation.py
@@ -4,20 +4,23 @@ import numpy as np
 
 #目的変数１つ用の交差検定関数,引数のfuncにはscikit-learnのモデルクラスを想定
 def cross_validation(func, data, times=10):
-    samples = random.sample(len(data))
-    newx = samples[:, :-1]
-    newy = samples[:, -1:]
+    random.shuffle(data)
+    newx = data[:, :-1]
+    newy = data[:, -1:]
 
-    sum=0
+    sum=0.0
     for i in range(times):
         accuracy=0
-        print("%d of %d" % (i, times))
+        print("%d of %d" % (i+1, times))
         crossnum = np.array([False] * len(data))
         crossnum[i * len(data) / 10:(i + 1) * len(data) / 10] = True
-        func.fit(newx[crossnum],newy[crossnum])
+        func.fit(newx[~crossnum],newy[~crossnum])
+        pred=func.predict(newx[crossnum])
+        test=newy[crossnum]
 
-        for j in func.predict(newx[~crossnum]):
-            if j==newy[~crossnum]:
+        for j in range(len(newx[crossnum])):
+            print ()
+            if pred[j]==test[j]:
                 accuracy+=1
         sum+=accuracy/len(newy[~crossnum])
     return sum / times
